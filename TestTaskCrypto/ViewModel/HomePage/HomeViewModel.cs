d631ece5c574bcb9d1385604aa2f68bae85e1987
nazar <nazar@NAZAR>
2025-06-16 16:26:48 +0300

    Add project files.

---
+++ TestTaskCrypto/ViewModel/HomePage/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskCrypto.Model.HomePage;
+
+namespace TestTaskCrypto.ViewModel.HomePage
+{
+    internal class HomeViewModel : ViewModel<HomeViewModel>
+    {
+        private HomeModel _model;
+
+        public HomeViewModel()
+        { 
+            _model = new HomeModel();
+        }
+
+    }
+}
